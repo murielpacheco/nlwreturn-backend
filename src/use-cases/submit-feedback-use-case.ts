9f5aa3a46feb4121f58349793798930eb56fbe53
Muriel Damazio <murieldps@gmail.com>
2022-05-08 19:10:20 -0300

    adjusting backend for deploy

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -39,6 +39,7 @@ class SubmitFeedbackUseCase {
             `<div style="font:family: Arial; font-size: 16px; color: #111">`,
                `<p>Tipo do feedback: ${type}</p>`,
                `<p>Comentário: ${comment}</p>`,
+               screenshot ? `<img src="${screenshot}" />` : "",
             `</div>`
          ].join('\n') 
       })
